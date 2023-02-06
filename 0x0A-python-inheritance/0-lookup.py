377d1fb39b3e6b5609bf817580dfc66fafcee9ac
kalwhyte <superkalel55@gmail.com>
2023-02-06 08:57:00 -0800

    task 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-'''Write a function that returns the list of available attributes and methods of an object:'''
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
+    """Return a list of an object's available attributes."""
     return (dir(obj))
