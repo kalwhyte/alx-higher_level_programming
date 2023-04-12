a6eb36075016a4dc62f247195a302bfed0f9a3b0
kalwhyte <superkalel55@gmail.com>
2023-04-12 23:13:12 +0100

    Debugging Task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,11 +2,13 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
-    this.value++;
-  }
 };
 console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
