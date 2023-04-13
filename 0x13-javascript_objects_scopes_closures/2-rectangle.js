adb3d91f89c5972746fba24604313ec421ad4891
kalwhyte <superkalel55@gmail.com>
2023-04-13 15:27:09 +0000

    The code seems to be missing the check for cases where w or h is not a positive integer or equal to 0, as mentioned in the instructions. If w or h is not a positive integer or equal to 0, the constructor should create an empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(Rectangle.prototype);
     }
-    this.width = w;
-    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 }
 
