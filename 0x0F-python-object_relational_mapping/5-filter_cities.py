f09ff4a2cf3297c136cf63bec11df8896eb09873
kalwhyte <superkalel55@gmail.com>
2023-04-17 00:55:16 +0100

    Printed fine but checker printing error, modified the script

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,11 +12,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
