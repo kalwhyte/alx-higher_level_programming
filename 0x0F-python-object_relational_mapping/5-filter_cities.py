0f109b0ef778dcbb6ab717180f7dbaed67488323
kalwhyte <superkalel55@gmail.com>
2023-04-17 00:12:11 +0100

    A script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""
-Displays all cities of a given state from the states table of the database hbtn_0e_4_usa.
+"""Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
 Safe from SQL injections.
-
-Usage: ./5-filter_cities.py <mysql username> <mysql password> <database name> <state name searched>
+Usage: ./filter_cities.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
 """
 
 import sys
@@ -11,21 +13,10 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db_username, db_password, db_name, state_name = sys.argv[1:5]
-
-    db_connection = MySQLdb.connect(user=db_username, passwd=db_password, db=db_name)
-    cursor = db_connection.cursor()
-
-    cursor.execute("SELECT `c`.`name` \
-                    FROM `cities` as `c` \
-                    INNER JOIN `states` as `s` \
-                    ON `c`.`state_id` = `s`.`id` \
-                    WHERE `s`.`name` = %s \
-                    ORDER BY `c`.`id`", (state_name,))
-
-    cities = [ct[0] for ct in cursor.fetchall()]
-
-    print(", ".join(cities))
-
-    cursor.close()
-    db_connection.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in cursor.fetchall() if ct[4] == sys.argv[4]]))
