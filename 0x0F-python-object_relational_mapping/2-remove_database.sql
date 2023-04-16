bdba1fce18516f68fe581eea020b412dcd98d6c5
kalwhyte <superkalel55@gmail.com>
2023-04-17 00:48:34 +0100

    a python file that contains the class definition of a State and an instance

--- 0x0F-python-object_relational_mapping/2-remove_database.sql
+++ 0x0F-python-object_relational_mapping/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Deletes the database hbtn_0e_0_usa from my MySQL server.
-DROP DATABASE IF EXISTS `hbtn_0e_0_usa`;
+DROP DATABASE IF EXISTS `hbtn_0e_666666_usa`;
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,8 +8,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(Base):
-    __tablename__ = "states"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+class State(Base):
+    """Represents a state for a MySQL database.
+
+    __tablename__ (str): The name of the MySQL table to store States
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
