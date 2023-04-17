4b19a67eb1c07a1d1ae72aae9d16e23334b79013
kalwhyte <superkalel55@gmail.com>
2023-04-17 17:45:37 +0100

    Made change to name convention, code checked but didn't meet standard

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,7 +5,6 @@ Inherits from SQLAlchemy Base and links to the MySQL table cities.
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
 
 Base = declarative_base()
 
@@ -19,7 +18,6 @@ class City(Base):
         state_id (sqlalchemy.String): The city's state id.
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    state_id = Column(Integer, ForeignKey("State.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/remove.sql
+++ 0x0F-python-object_relational_mapping/remove.sql
@@ -1,2 +1,2 @@
 -- Deletes the database hbtn_0e_0_usa from my MySQL server.
-DROP DATABASE IF EXISTS `hbtn_0e_6_usa`;
+DROP DATABASE IF EXISTS `hbtn_0e_101_usa`;
