4208ccb090f290f35b70f1c0a563323533bd054b
kalwhyte <superkalel55@gmail.com>
2023-04-28 22:37:55 +0000

    Task 0: Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+
+def fetch_url(url):
+    """
+    Fetches the contents of the given URL.
+
+    Args:
+        url: A string representing the URL to fetch.
+
+    Returns:
+        The body of the HTTP response.
+    """
+    with urllib.request.urlopen(url) as response:
+        return response.read()
+
+
+def display_info(response):
+    """
+    Displays information about the HTTP response.
+
+    Args:
+        response: A bytes object representing the body of the HTTP response.
+    """
+    body = response.decode("utf-8")
+    print("- type: {}".format(type(body)))
+    print("- content: {}".format(body))
+    print("- utf8 content: {}".format(body))
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response_body = fetch_url(url)
+    display_info(response_body)
