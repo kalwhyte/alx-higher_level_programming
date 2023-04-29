e2fb6dd7df233a36e0daa3aac9f59d9b7a624eec
kalwhyte <superkalel55@gmail.com>
2023-04-29 01:28:11 +0000

    Python script that takes 2 arguments in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,21 +8,15 @@ import requests
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./100-github_commits.py <repository name> <repository owner>")
-        sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()
-
-        for commit in commits[:10]:
-            sha = commit.get("sha")
-            author_name = commit.get("commit").get("author").get("name")
-            print(f"{sha}: {author_name}")
-
-    else:
-        print(f"Error: {response.status_code}")
-        sys.exit(1)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
