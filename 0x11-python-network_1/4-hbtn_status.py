05eefabaa946675a8c519505da5fd521c1138384
kalwhyte <superkalel55@gmail.com>
2023-04-29 00:29:47 +0000

    Displays the X-Request-Id header variable of a request to a given URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Script that fetches https://alx-intranet.hbtn.io/status"""
+"""fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
-if __name__ == '__main__':
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
+if __name__ == "__main__":
+    response = requests.get(url("https://alx-intranet.hbtn.io/status"))
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
