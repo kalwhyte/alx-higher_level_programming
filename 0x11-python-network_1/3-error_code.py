0cbcaa52c68881431fbed7f4e4e3f3727353f297
kalwhyte <superkalel55@gmail.com>
2023-04-29 00:06:54 +0000

    A Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-'''
-Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
-'''
+'''sends a request to the URL and displays the body of the response.
 
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+'''
 import urllib.request
 import urllib.error
 import sys
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-'''
-Write a Python script that fetches https://alx-intranet.hbtn.io/status
-'''
+'''Script that fetches https://alx-intranet.hbtn.io/status'''
 import requests
+
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
