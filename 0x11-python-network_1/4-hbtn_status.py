77d86b3820001fc38f082b356d2b5d9f00aae02a
kalwhyte <superkalel55@gmail.com>
2023-04-29 00:23:07 +0000

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-'''Script that fetches https://alx-intranet.hbtn.io/status'''
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
-    print('Body response:')
-    print('\t- type:', type(response.text))
-    print('\t- content:', response.text)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
